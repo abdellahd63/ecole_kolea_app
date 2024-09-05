dce458afbda04888e370363867ffb25ed9644e61
Abdallah DEKKICHE <119437598+abdellahd63@users.noreply.github.com>
2024-09-05 10:31:40 +0100

    fgfdh

--- lib/Constant.dart
+++ lib/Constant.dart
@@ -1,5 +1,5 @@
 class Constant{
- //static const URL = 'https://esce.streamsystem.com';
- static const URL = 'http://192.168.100.67:8000';
+ static const URL = 'https://esce.streamsystem.com';
+ //static const URL = 'http://192.168.100.67:8000';
  static const Key = 'JK~pDeuzM8J87sg4';
 }
